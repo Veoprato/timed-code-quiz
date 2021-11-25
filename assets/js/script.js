090bb336563ce684571ea2f6ad5c54f32f2565df
Veoprato <75872616+Veoprato@users.noreply.github.com>
2021-11-25 04:06:09 -0500

    added Final Score container in index.html; added variable for page containers in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('startbutton');
+const startContainer = document.getElementById('mainpage')
+const quizContainer = document.getElementById('quizcontainer');
+const endContainer = document.getElementById('endgame')
+const resultsContainer = document.getElementById('highscores');
