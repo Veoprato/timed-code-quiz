1b871a71ecd723054c2fef83815baed71f85cb39
Veoprato <75872616+Veoprato@users.noreply.github.com>
2021-11-25 04:29:30 -0500

    Add quiz container; Add array for questions; Some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
+// Variables 
 const startButton = document.getElementById('startbutton');
 const startContainer = document.getElementById('mainpage')
-const quizContainer = document.getElementById('quizcontainer');
+const quizContainer = document.getElementById('quiz-container');
 const endContainer = document.getElementById('endgame')
 const resultsContainer = document.getElementById('highscores');
+
+
+// Quiz Questions Array 
+const quizQuestions = [
+    {
+        q: "Commonly used datatypes DO NOT include:",
+        choices: ['1. string', '2. boolean', '3. alerts', '4. numbers'],
+        a: "3. alerts"
+    },
+    {
+        q: "The condition in an if/else statement is enclosed with ____.",
+        choices: ['1. quotes', '2. curly braces', '3. parenthesis', '4. square brackets'],
+        a: "3. parenthesis"
+    },
+    {
+        q: "Arrays in javascript can be used to store ____.",
+        choices: ['1. numbers an strings', '2. booleans', '3. other arrays', '4. all of the above'],
+        a: "4. all of the above"
+    },
+    {
+        q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+        a: "4. console.log"
+    }
+];
